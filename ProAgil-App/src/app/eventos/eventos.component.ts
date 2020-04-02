9924dfc88682fead509285bb58b0fd4b0e27e503
Eduardo Barros <barrosebs@gmail.com>
2020-04-01 23:19:16 -0300

    add bootstrap e fontawser ao projeto

--- ProAgil-App/src/app/eventos/eventos.component.ts
+++ ProAgil-App/src/app/eventos/eventos.component.ts
@@ -1,33 +1,27 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-eventos',
   templateUrl: './eventos.component.html',
   styleUrls: ['./eventos.component.css']
 })
+
 export class EventosComponent implements OnInit {
 
-  eventos: any = [
-    {
-      EventoId: 1,
-      Tema: 'Angular',
-      Local: 'Belo Horizonte'
-    },
-    {
-      EventoId: 2,
-      Tema: '.NET Core',
-      Local: 'São Paulo'
-    },
-    {
-      EventoId: 3,
-      Tema: 'Angular e .NET Core',
-      Local: 'Rio de Janeiro'
-    }
-  ];
+  eventos: any = [];
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.getEventos();
   }
 
+  getEventos() {
+    this.eventos = this.http.get('http://localhost:5000/api/values').subscribe(
+      response => {
+        this.eventos = response;
+      }, error => {
+        console.log(error);
+      });
+  }
 }
