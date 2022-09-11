f920317982d4200de6051f571c8c95a6405c181e
Eduardo Barros <barrosebs@gmail.com>
2022-09-11 11:30:06 -0300

    swagger inserted and configuranted

--- ProAgil.WebAPI/Startup.cs
+++ ProAgil.WebAPI/Startup.cs
@@ -1,9 +1,13 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
 using ProAgil.Repository;
 
 
@@ -21,12 +25,26 @@ namespace ProAgil.WebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
             services.AddDbContext<ProAgilContext>(x =>
                 x.UseSqlite(Configuration.GetConnectionString("DefaultConnection"))
                 );
             services.AddScoped<IProAgilRepository, ProAgilRepository>();
             services.AddMvc();
             services.AddCors();
+            services.AddSwaggerGen(c =>
+                                       {
+                                           c.SwaggerDoc("v1", new OpenApiInfo
+                                           {
+                                               Title = "Curso Udemy - Dev FullStack with Angular",
+                                               Version = "v1"
+                                           });
+
+                                           var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                                           var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+
+                                           c.IncludeXmlComments(xmlPath, includeControllerXmlComments: true);
+                                       });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,16 +55,25 @@ namespace ProAgil.WebAPI
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            app.UseStaticFiles();
+            app.UseHttpsRedirection();
             app.UseRouting();
+            app.UseAuthorization();
+            app.UseStaticFiles();
             app.UseCors(access => access.AllowAnyHeader()
                 .AllowAnyMethod()
                 .AllowAnyOrigin());
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}");
+                endpoints.MapControllers();
+            });
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.RoutePrefix = string.Empty;
+                c.SwaggerEndpoint("../swagger/v1/swagger.json", "v1");
             });
 
         }
